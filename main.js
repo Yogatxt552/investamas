8d5d82ae15776337f7e73e0132d97e9593b2c0c6
fanani03 <afanfanani202@gmail.com>
2021-05-27 01:07:08 +0700

    test, membuat inputan pembelian

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+$(document).ready(function(){
+    var totalBeli = 0
+    var gr = 0
+	$("#m").click(function(){
+	  	
+		var n = $("#n").val();
+		
+	    for ( var i = 1; i <= n; i++) {
+	    	$("#input").append(" Nominal Beli (IDR) <input type='text' id='beli"+i+"'><br>");
+	    	$("#input").append(" Harga Beli (IDR)<input type='text' id='jual"+i+"'><br>");
+	    }
 
+	    $("#input").append("Harga Jual <input type='text' id='jual'><br>");
+	    $("#input").append("<button id='hitung'>HITUNG</button>");
+	
+    });
+
+});
